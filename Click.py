e589de396b19b24d66effc840f7d53d291a9de4d
Bedirhan Alparslan <65812711+DRYourself@users.noreply.github.com>
2021-02-26 21:34:56 +0300

    AutoClick

---
+++ Click.py
@@ -0,0 +1,27 @@
+import keyboard  # using module keyboard
+import pyautogui
+import time
+
+from win10toast import ToastNotifier
+
+toaster = ToastNotifier()
+
+print("Click \" to open autoclick")
+print("Click again \" to close autoclick")
+
+aaa=False
+while True:  # making a loop
+    try:  # used try so that if user pressed other than the given key error will not be shown
+        if keyboard.is_pressed('"'):  # if key pressed 
+            if aaa==True:
+                aaa=False
+                toaster.show_toast("False")
+                time.sleep(1)
+            else:
+                aaa=True
+                toaster.show_toast("True")
+                time.sleep(1)
+        if aaa==True:
+            pyautogui.click()
+    except:
+        pass
